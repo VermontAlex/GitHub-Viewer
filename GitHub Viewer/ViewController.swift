e18ccfcbbd75482b54b3f856360f1852d973dba9
Oleksandr Oliinyk <oleksandr.oliinyk@ooliinyk-mbp.local>
2022-06-06 11:35:25 +0300

    Init commit

---
+++ GitHub Viewer/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  GitHub Viewer
+//
+//  Created by Oleksandr Oliinyk on 06.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
